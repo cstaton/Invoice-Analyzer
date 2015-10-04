bf1c8a68ab20cc49f58091c2326e1530fb7108f7
Chris <cpa.staton@gmail.com>
2015-10-03 19:33:33 -0700

    update server

--- server/myServer.js
+++ server/myServer.js
@@ -3,10 +3,16 @@ var mongoose = require("mongoose");
 
 var app = express();
 
-mongoose.connect("mongodb://localhost/plastiq")
+mongoose.connect(process.env.MONGOLAB_URI, function(error) {
+	if (error) {
+		console.error(error);
+	} else {
+		console.log("Mongo Connected!!");
+	}
+});
 
 require("./middleware")(app, express);
 
 
 app.listen(process.env.PORT || 3030);
-console.log("listening on port 3030")
+console.log("listening on port 3030");
